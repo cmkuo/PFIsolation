095d2dbed3a6e53f1bef67f1b7a63ec768f19574
cmkuo <cmkuo@mail.cern.ch>
2013-08-17 22:58:57 +0200

    first commit

---
+++ SuperClusterFootprintRemoval/interface/SuperClusterFootprintRemoval.h
@@ -0,0 +1,137 @@
+// -*- C++ -*-
+//
+// Package:    SuperClusterFootprintRemoval
+// Class:      SuperClusterFootprintRemoval
+// 
+/**\class SuperClusterFootprintRemoval SuperClusterFootprintRemoval.cc PFIsolation/SuperClusterFootprintRemoval/plugins/SuperClusterFootprintRemoval.cc
+
+ Description: Implements the algo for removal of PF clusters from the SC footprint
+
+ Implementation:
+     Runs on AOD.
+*/
+//
+// Original Author:  Marco Peruzzi,32 4-C16,+41227676829,
+//         Created:  Sat Sep 29 17:58:21 CEST 2012
+// $Id: SuperClusterFootprintRemoval.h,v 1.1 2013/05/27 08:15:50 cmkuo Exp $
+//
+//
+
+
+// system include files
+#include <memory>
+#include <iostream>
+#include <cmath>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+
+#include "Geometry/CaloGeometry/interface/CaloGeometry.h"
+#include "Geometry/Records/interface/CaloGeometryRecord.h"
+#include "Geometry/CaloGeometry/interface/CaloSubdetectorGeometry.h"
+#include "Geometry/CaloTopology/interface/CaloTopology.h"
+#include "Geometry/CaloEventSetup/interface/CaloTopologyRecord.h"
+#include "DataFormats/GeometryVector/interface/GlobalPoint.h"
+#include "MagneticField/Engine/interface/MagneticField.h"
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
+#include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
+#include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/EcalDetId/interface/EBDetId.h"
+#include "DataFormats/EcalDetId/interface/EEDetId.h"
+#include "DataFormats/CaloRecHit/interface/CaloCluster.h"
+#include "DataFormats/CaloRecHit/interface/CaloClusterFwd.h"
+#include "Geometry/EcalAlgo/interface/EcalBarrelGeometry.h"
+#include "Geometry/EcalAlgo/interface/EcalEndcapGeometry.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidateFwd.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
+#include "DataFormats/GsfTrackReco/interface/GsfTrack.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+#include "TGeoTube.h"
+#include "TGeoPara.h"
+#include "TVector3.h"
+#include "TMath.h"
+#include "TRotation.h"
+#include "TString.h"
+
+#ifndef __SUPERCLUSTERFOOTPRINTREMOVAL__HH__
+#define __SUPERCLUSTERFOOTPRINTREMOVAL__HH__
+
+
+//
+// constants, enums and typedefs
+//
+
+typedef struct {
+  int nxtals;
+  std::vector<TVector3> xtalposition;
+  std::vector<float> xtaletawidth;
+  std::vector<float> xtalphiwidth;
+  std::vector<std::vector<TVector3> > xtalcorners;
+} sc_xtal_information;
+
+typedef struct {
+  float dR;
+  float dEta;
+  float dPhi;
+} angular_distances_struct;
+
+
+//
+// class declaration
+//
+
+class SuperClusterFootprintRemoval {
+
+public:
+
+//    The constructor can take the following arguments from configuration (default value):
+//
+//    isolation_cone_size_forSCremoval (0.4) : isolation cone used for PF Iso calculation
+//    tag_pfCandidates_forSCremoval ("particleFlow") : collection of PF candidates to use
+//    tag_Vertices_forSCremoval ("offlinePrimaryVertices") : collection of vertices to use
+//    rechit_link_enlargement_forSCremoval (0.25) : enlargement of linear dimension of xtals for rechit matching
+
+  SuperClusterFootprintRemoval(const edm::Event& iEvent, const edm::ParameterSet& iConfig, const edm::EventSetup& iSetup);
+  ~SuperClusterFootprintRemoval();
+
+  // Calculate the PF isolation around the object consisting of the SuperCluster sc. Component can be "neutral","charged" or "photon".
+  // The vertexforchargediso parameter should be passed for charged isolation ONLY, and tells with respect to which vertex (within the vertices collection) we should cut on dxy (0.1 cm) and dz (0.2 cm) of the track associated to charged PF candidate. Ideally, this should be the vertex from which the object consisting of the SuperCluster sc comes from. To turn off this selection, allowing charged PF candidates from all vertices to enter the isolation sum, pass vertexforchargediso=-1.
+  float PFIsolation(TString component, reco::SuperClusterRef sc, int vertexforchargediso=-999);
+
+  // Get the vector of the indices of those PF candidates (neutrals,charged and photons, in the pfCandidates collection) that are inside the SC footprint or are duplicata of the RECO object with SuperCluster sc
+  std::vector<int> GetPFCandInFootprint(reco::SuperClusterRef sc);
+
+  // Get the angular distance between the pf candidate pfCandidates[pfindex] and the SuperCluster sc
+  angular_distances_struct GetPFCandHitDistanceFromSC(reco::SuperClusterRef sc, int pfindex);
+
+private:
+
+  TVector3 PropagatePFCandToEcal(int pfcandindex, float position, bool isbarrel);
+  sc_xtal_information GetSCXtalInfo(reco::SuperClusterRef sc);
+  std::vector<int> GetMatchedPFCandidates(reco::SuperClusterRef sc);
+
+  CaloSubdetectorGeometry *barrelGeometry;
+  CaloSubdetectorGeometry *endcapGeometry;
+  TGeoPara eegeom;
+  MagneticField *magField;
+
+  edm::Handle<reco::PFCandidateCollection> pfCandidates;  
+  edm::Handle<reco::VertexCollection> vertexHandle;
+
+  double global_linkbyrechit_enlargement;
+  double global_isolation_cone_size;
+
+  int FindPFCandType(int id);
+  
+};
+
+#endif
+
---
+++ SuperClusterFootprintRemoval/src/SuperClusterFootprintRemoval.cc
@@ -0,0 +1,380 @@
+// -*- C++ -*-
+//
+// Package:    SuperClusterFootprintRemoval
+// Class:      SuperClusterFootprintRemoval
+// 
+/**\class SuperClusterFootprintRemoval SuperClusterFootprintRemoval.cc PFIsolation/SuperClusterFootprintRemoval/plugins/SuperClusterFootprintRemoval.cc
+
+ Description: Implements the algo for removal of PF clusters from the SC footprint
+
+ Implementation:
+     Runs on AOD.
+*/
+//
+// Original Author:  Marco Peruzzi,32 4-C16,+41227676829,
+//         Created:  Sat Sep 29 17:58:21 CEST 2012
+// $Id: SuperClusterFootprintRemoval.cc,v 1.2 2013/05/27 08:16:15 cmkuo Exp $
+//
+//
+
+
+#ifndef __SUPERCLUSTERFOOTPRINTREMOVAL__CC__
+#define __SUPERCLUSTERFOOTPRINTREMOVAL__CC__
+
+#include "../interface/SuperClusterFootprintRemoval.h"
+
+//
+// constructors and destructor
+//
+SuperClusterFootprintRemoval::SuperClusterFootprintRemoval(const edm::Event& iEvent, const edm::ParameterSet& iConfig, const edm::EventSetup& iSetup)
+{
+   //now do what ever initialization is needed
+
+  eegeom = TGeoPara(1,1,1,0,0,0);
+
+  global_isolation_cone_size = iConfig.getUntrackedParameter<double>("isolation_cone_size_forSCremoval",0.3);
+  global_linkbyrechit_enlargement = iConfig.getUntrackedParameter<double>("rechit_link_enlargement_forSCremoval",0.25);
+
+  edm::ESHandle<CaloGeometry> geometry ;
+  iSetup.get<CaloGeometryRecord>().get(geometry);
+  barrelGeometry = (CaloSubdetectorGeometry*)(geometry->getSubdetectorGeometry(DetId::Ecal, EcalBarrel));
+  endcapGeometry = (CaloSubdetectorGeometry*)(geometry->getSubdetectorGeometry(DetId::Ecal, EcalEndcap));
+
+  edm::ESHandle<MagneticField> magneticField;
+  iSetup.get<IdealMagneticFieldRecord>().get(magneticField);
+  magField = (MagneticField*)(magneticField.product());
+
+  //PFcandidates
+  edm::InputTag pfCandidateTag = iConfig.getUntrackedParameter<edm::InputTag>("tag_pfCandidates_forSCremoval",edm::InputTag("particleFlow"));
+  iEvent.getByLabel(pfCandidateTag, pfCandidates);
+
+  //Vertices
+  edm::InputTag fVertexTag = iConfig.getUntrackedParameter<edm::InputTag>("tag_Vertices_forSCremoval",edm::InputTag("offlinePrimaryVertices"));
+  iEvent.getByLabel(fVertexTag, vertexHandle);
+
+}
+
+
+SuperClusterFootprintRemoval::~SuperClusterFootprintRemoval()
+{
+ 
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+
+}
+
+
+//
+// member functions
+//
+
+
+TVector3 SuperClusterFootprintRemoval::PropagatePFCandToEcal(int pfcandindex, float position, bool isbarrel){
+  // WARNING: this propagates until EE+ or EE- at the given TMath::Abs(position.z()) for isbarrel=0, depending on where the candidate is pointing.
+
+  if (!((*pfCandidates)[pfcandindex].pt()>0)) {
+    std::cout << "Warning: called propagation to ECAL for object with negative or zero pt. Returning TVector3(0,0,1e10)." << std::endl;
+    return TVector3(0,0,1e10);
+  }
+
+  int i = pfcandindex;
+  int type = FindPFCandType((*pfCandidates)[i].pdgId());
+
+  if (type>2) {
+    std::cout << "Asking propagation for lepton, not implemented. Returning TVector3(0,0,1e10)." << std::endl;
+    return TVector3(0,0,1e10);
+  }
+
+  TVector3 pfvertex((*pfCandidates)[i].vx(),(*pfCandidates)[i].vy(),(*pfCandidates)[i].vz());
+  TVector3 pfmomentum((*pfCandidates)[i].px(),(*pfCandidates)[i].py(),(*pfCandidates)[i].pz());
+  pfmomentum = pfmomentum.Unit();
+  TVector3 ecalpfhit(0,0,0);
+  if (isbarrel){
+    double p[3] = {pfvertex.x(),pfvertex.y(),pfvertex.z()};
+    double d[3] = {pfmomentum.x(),pfmomentum.y(),pfmomentum.z()};
+    double dist = TGeoTube::DistFromInsideS(p,d,0,position,1e+10);
+    ecalpfhit = pfvertex + dist*pfmomentum;
+  }
+  else { // EE
+    double dim[6]={1e+10,1e+10,fabs(position),0,0,0};
+    double p[3] = {pfvertex.x(),pfvertex.y(),pfvertex.z()};
+    double d[3] = {pfmomentum.x(),pfmomentum.y(),pfmomentum.z()};
+    eegeom.SetDimensions(dim);
+    double dist = eegeom.DistFromInside(p,d);
+    ecalpfhit = pfvertex + dist*pfmomentum;
+  }
+
+
+  if (type==1){ // APPROXIMATE helix propagation
+
+    float field = magField->inTesla(GlobalPoint(0.,0.,0.)).z();
+    float charge = (*pfCandidates)[i].charge();
+    float curvature = ((*pfCandidates)[i].pt())/(0.3*field*charge)*100; // curvature radius in cm
+    float final_radius = ecalpfhit.Perp();
+    float addphi = -TMath::ASin(final_radius/curvature/2);
+
+    TRotation r;
+    r.RotateZ(addphi);
+    ecalpfhit *= r;
+
+  }
+
+
+  return ecalpfhit;
+
+}
+
+sc_xtal_information SuperClusterFootprintRemoval::GetSCXtalInfo(reco::SuperClusterRef sc){
+
+  sc_xtal_information out;
+
+  std::vector<DetId> cristalli;         
+  for (reco::CaloCluster_iterator bc=sc->clustersBegin(); bc!=sc->clustersEnd(); ++bc){
+    const std::vector< std::pair<DetId, float> > & seedrechits = (*bc)->hitsAndFractions();
+    for (unsigned int i=0; i<seedrechits.size(); i++) cristalli.push_back(seedrechits[i].first);
+    sort(cristalli.begin(),cristalli.end());
+    std::vector<DetId>::iterator it;
+    it = unique(cristalli.begin(),cristalli.end());
+    cristalli.resize(it-cristalli.begin());
+  }
+          
+  unsigned int i=0;
+  for (i=0; i<cristalli.size(); i++){
+
+    if (cristalli.at(i).subdetId()!=EcalBarrel && cristalli.at(i).subdetId()!=EcalEndcap) continue;
+    bool isbarrel = (cristalli.at(i).subdetId()==EcalBarrel);
+
+    CaloCellGeometry *cellGeometry = NULL;
+    EBDetId ebDetId;
+    EEDetId eeDetId;
+    if (isbarrel) {
+      ebDetId = cristalli.at(i); 
+      cellGeometry = (CaloCellGeometry*)(barrelGeometry->getGeometry(ebDetId));
+    }
+    else {
+      eeDetId = cristalli.at(i);
+      cellGeometry = (CaloCellGeometry*)(endcapGeometry->getGeometry(eeDetId));
+    }
+    TVector3 xtal_position(cellGeometry->getPosition().x(),cellGeometry->getPosition().y(),cellGeometry->getPosition().z());
+
+    float dphi; float deta;
+    if (isbarrel){
+    dphi=(dynamic_cast<const EcalBarrelGeometry*>(barrelGeometry))->deltaPhi(ebDetId);
+    deta=(dynamic_cast<const EcalBarrelGeometry*>(barrelGeometry))->deltaEta(ebDetId);
+    }
+    else {
+    dphi=(dynamic_cast<const EcalEndcapGeometry*>(endcapGeometry))->deltaPhi(eeDetId);
+    deta=(dynamic_cast<const EcalEndcapGeometry*>(endcapGeometry))->deltaEta(eeDetId);
+    }
+    
+    const CaloCellGeometry::CornersVec& cellCorners (cellGeometry->getCorners());
+    std::vector<TVector3> corners;
+    for (int k=0; k<4; k++){
+      TVector3 thiscorner((float)(cellCorners[k].x()),(float)(cellCorners[k].y()),(float)(cellCorners[k].z()));
+      corners.push_back(thiscorner);
+    }
+    out.xtalposition.push_back(xtal_position);
+    out.xtaletawidth.push_back(deta);
+    out.xtalphiwidth.push_back(dphi);
+    out.xtalcorners.push_back(corners);
+  }
+  out.nxtals=i;
+
+  assert (out.nxtals==(int)out.xtalposition.size());
+
+  return out;
+
+}
+
+std::vector<int> SuperClusterFootprintRemoval::GetMatchedPFCandidates(reco::SuperClusterRef sc){
+
+  std::vector<int> out;
+
+  for (unsigned int i=0; i<pfCandidates->size(); i++) {
+    if ((*pfCandidates)[i].pdgId()==22) {
+      if ((*pfCandidates)[i].mva_nothing_gamma()>0){
+	if( (*pfCandidates)[i].superClusterRef()==sc) {
+	  out.push_back(i);
+	}
+      }
+    }
+  }
+
+  return out;
+
+}
+
+std::vector<int> SuperClusterFootprintRemoval::GetPFCandInFootprint(reco::SuperClusterRef sc){
+
+  bool isbarrel = (fabs(sc->eta())<1.5);
+
+  sc_xtal_information infos = GetSCXtalInfo(sc);
+  std::vector<int> matchedpfcand = GetMatchedPFCandidates(sc);
+
+  std::vector<int> result;
+
+  for (unsigned int i=0; i<pfCandidates->size(); i++){
+
+    if (!((*pfCandidates)[i].pt()>0)) continue;
+
+    int type = FindPFCandType((*pfCandidates)[i].pdgId());
+    if (type>2) continue;
+
+    bool inside=false;
+
+    for (unsigned int j=0; j<matchedpfcand.size(); j++) if ((int)i==matchedpfcand.at(j)) inside=true;
+
+    for (int j=0; j<infos.nxtals; j++){
+      
+      TVector3 xtal_position = infos.xtalposition[j];
+      TVector3 ecalpfhit = PropagatePFCandToEcal(i,isbarrel ? xtal_position.Perp() : xtal_position.z(), isbarrel);
+
+      if (ecalpfhit.Perp()==0) continue;
+
+      if (isbarrel){
+	float xtalEtaWidth = infos.xtaletawidth[j]*(1.0+global_linkbyrechit_enlargement);
+	float xtalPhiWidth = infos.xtalphiwidth[j]*(1.0+global_linkbyrechit_enlargement);
+	float deta = ecalpfhit.Eta()-xtal_position.Eta();
+	float dphi = reco::deltaPhi(ecalpfhit.Phi(),xtal_position.Phi());
+	if (fabs(deta)<xtalEtaWidth/2 && fabs(dphi)<xtalPhiWidth/2) inside=true;
+      }
+      else { // EE
+	if (ecalpfhit.z()*xtal_position.z()>0){
+	  TVector3 xtal_corners[4];
+	  for (int k=0; k<4; k++) xtal_corners[k] = infos.xtalcorners[j][k];
+	  float hitx = ecalpfhit.x();
+	  float hity = ecalpfhit.y();
+	  float polx[5];
+	  float poly[5];
+	  for (int k=0; k<4; k++) polx[k] = xtal_corners[k].x();
+	  for (int k=0; k<4; k++) poly[k] = xtal_corners[k].y();
+	  polx[4]=polx[0]; poly[4]=poly[0]; // closed polygon
+	  float centerx = (polx[0]+polx[1]+polx[2]+polx[3])/4;
+	  float centery = (poly[0]+poly[1]+poly[2]+poly[3])/4;
+	  hitx = centerx + (hitx-centerx)/(1.0+global_linkbyrechit_enlargement);
+	  hity = centery + (hity-centery)/(1.0+global_linkbyrechit_enlargement);
+	  if (TMath::IsInside(hitx,hity,5,polx,poly)) inside=true;
+	}
+      }
+
+    }
+
+    if (inside) result.push_back(i);
+
+  }
+
+  return result;
+
+}
+
+
+angular_distances_struct SuperClusterFootprintRemoval::GetPFCandHitDistanceFromSC(reco::SuperClusterRef sc, int pfindex){
+
+  int i = pfindex;
+  int type = FindPFCandType((*pfCandidates)[i].pdgId());
+
+  if (type>2) {
+    std::cout << "propagation not implemented for lepton objects!!!" << std::endl;
+    angular_distances_struct out;
+    out.dR=999; out.dEta=999; out.dPhi=999;
+    return out;
+  }
+
+  bool isbarrel = (fabs(sc->eta())<1.5);
+
+  TVector3 sc_position = TVector3(sc->x(),sc->y(),sc->z());
+
+  TVector3 pfvertex((*pfCandidates)[i].vx(),(*pfCandidates)[i].vy(),(*pfCandidates)[i].vz());
+  TVector3 pfmomentum((*pfCandidates)[i].px(),(*pfCandidates)[i].py(),(*pfCandidates)[i].pz());
+  pfmomentum = pfmomentum.Unit();
+
+  TVector3 ecalpfhit = PropagatePFCandToEcal(i,isbarrel ? sc_position.Perp() : sc_position.z(),isbarrel);
+
+  if (ecalpfhit.Perp()==0){
+    std::cout << "GetPFCandHitDistanceFromSC: impact position found in the origin of the transverse plane. Returning error state." << std::endl;
+    angular_distances_struct out;
+    out.dR = 999;
+    out.dEta = 999;
+    out.dPhi = 999;
+    return out;
+  }
+
+  angular_distances_struct out;
+  out.dR = reco::deltaR(sc_position.Eta(),sc_position.Phi(),ecalpfhit.Eta(),ecalpfhit.Phi());
+  out.dEta = ecalpfhit.Eta()-sc_position.Eta();
+  out.dPhi = reco::deltaPhi(ecalpfhit.Phi(),sc_position.Phi());
+
+  return out;
+
+}
+
+int SuperClusterFootprintRemoval::FindPFCandType(int id){
+
+  int type = -1;
+
+  if (id==111 || id==130 || id==310 || id==2112) type=0; //neutral hadrons
+  if (fabs(id)==211 || fabs(id)==321 || id==999211 || fabs(id)==2212) type=1; //charged hadrons
+  if (id==22) type=2; //photons
+  if (fabs(id)==11) type=3; //electrons
+  if (fabs(id)==13) type=4; //muons
+
+  return type;
+}
+
+float SuperClusterFootprintRemoval::PFIsolation(TString component, reco::SuperClusterRef sc, int vertexforchargediso){
+
+  if (component!="charged") {
+    if (vertexforchargediso!=-999) std::cout << "WARNING: Why are you specifying a vertex for neutral or photon isolation? This will be ignored." << std::endl;
+    vertexforchargediso=-1;
+  }
+
+  if (component=="charged") {
+      if (vertexforchargediso==-999) {std::cout << "WARNING: You did not specify a vertex for the charged component of PF isolation. Deactivating vertex cuts (vertexforchargediso=-1) by default." << std::endl; vertexforchargediso=-1;}
+      if (vertexforchargediso > (int)(vertexHandle->size())-1 || vertexforchargediso<-1) {std::cout << "ERROR: Invalid vertexforchargediso specified. Returning 999." << std::endl; return 999;}
+    }
+
+  float result = 0;
+  std::vector<int> removed = GetPFCandInFootprint(sc);
+
+  int thistype=-999;
+  if (component=="neutral") thistype=0;
+  if (component=="charged") thistype=1;
+  if (component=="photon")  thistype=2;
+
+  if (thistype==-999) {std::cout << "ERROR: Incorrect PF isolation component selected. Returning 999." << std::endl; return 999;}
+
+  for (unsigned int i=0; i<pfCandidates->size(); i++){
+
+    if (!((*pfCandidates)[i].pt()>0)) continue;
+
+    int type = FindPFCandType((*pfCandidates)[i].pdgId());
+    if (type!=thistype) continue;
+
+    bool toremove = false;
+    for (unsigned int j=0; j<removed.size(); j++) if ((int)i==removed.at(j)) toremove = true;
+    if (toremove) continue;
+
+    angular_distances_struct distance = GetPFCandHitDistanceFromSC(sc,i);
+    if (distance.dR>global_isolation_cone_size) continue;
+
+    if (type==1 && vertexforchargediso>-1){
+      TVector3 pfvertex((*pfCandidates)[i].vx(),(*pfCandidates)[i].vy(),(*pfCandidates)[i].vz());
+      TVector3 vtxmom((*pfCandidates)[i].trackRef()->px(),(*pfCandidates)[i].trackRef()->py(),(*pfCandidates)[i].trackRef()->pz());
+      TVector3 phovtx((*vertexHandle)[vertexforchargediso].x(),(*vertexHandle)[vertexforchargediso].y(),(*vertexHandle)[vertexforchargediso].z());
+      float dxy = ( -(pfvertex.x()-phovtx.x())*vtxmom.y() +(pfvertex.y()-phovtx.y())*vtxmom.x() ) / vtxmom.Perp();
+      float dz = (pfvertex.z()-phovtx.z()) - ( (pfvertex.x()-phovtx.x())*vtxmom.x() + (pfvertex.y()-phovtx.y())*vtxmom.y() ) / vtxmom.Perp() * vtxmom.z() / vtxmom.Perp();
+      dxy=fabs(dxy);
+      dz=fabs(dz);
+      if (dz>0.2) continue;
+      if (dxy>0.1) continue;
+    }
+
+    result+=(*pfCandidates)[i].pt();
+
+  }
+
+  return result;
+
+}
+
+#endif
